b219e11c2c7a283407d7223c2addb5f4a9997a18
krystian3 <krystian.jaworowski@infotech.edu.pl>
2022-09-19 12:21:02 +0200

    Initial commit

---
+++ my_blog/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    is_open_question = models.BooleanField(default=False)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Answer(models.Model):
+    answer = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)

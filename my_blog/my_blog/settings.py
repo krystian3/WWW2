c82ffea7407e7570d1c1ab17bdda56d72e7d5779
krystian3 <krystian.jaworowski@infotech.edu.pl>
2022-10-10 19:50:49 +0200

    WWW 4.1 Widoki i szablony

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'polls.urls'
+ROOT_URLCONF = 'my_blog.urls'
 
 TEMPLATES = [
     {
---
+++ my_blog/polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
+
+def question_list(request):
+    questions = Question.objects.all()
+    return render(request, 'polls/question_list.html', {'questions': questions})
+
+
+def question_detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/question_detail.html', {'question': question})

f6eed967800d92ac943c94d4dec3b96c74652688
krystian3 <krystian.jaworowski@infotech.edu.pl>
2022-10-24 10:15:03 +0200

    4.2

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home_view
 
 
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls', namespace='polls')),
 ]
